0c44896db3a151789e7ac4f0d734f0f98385b0ee
koverton <ken.overton@gmail.com>
2017-12-20 18:10:54 -0500

    simplify mvn xml configs

--- mvn_libs/install.sh
+++ mvn_libs/install.sh
@@ -1,11 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash
 #
 # Run this script to install this Solace SEMP parser in your local MVN repository.
 # These versions are referenced in the default Maven pom.xml for this project.
 #
 # To parse other versions of the SEMP v1 (XML-based) protocol, see https://github.com/koverton/semp_jaxb
 
-mvn install:install-file -Dfile=semp_jaxb_8_2_0-8.2.1.4.jar     -DpomFile=semp_jaxb_8_2_0-8.2.1.4.pom
-
-mvn install:install-file -Dfile=semp_jaxb_8_6VMR-8.6.0.1010.jar -DpomFile=semp_jaxb_8_6VMR-8.6.0.1010.pom
-
+for JAR in *.jar; do
+	POM=`echo $JAR | sed 's/jar/pom/'`
+	if [ -e "$POM" ]; then
+		echo "POM! $POM"
+		mvn install:install-file -Dfile=$JAR     -DpomFile=$POM
+	else
+		echo "!!!!!!!!! MISSING???? $POM"
+	fi
+done
